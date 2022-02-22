07101b588db3cd764c18ba167ec31ea681c48e45
SinghAjay06 <92781365+SinghAjay06@users.noreply.github.com>
2022-02-22 14:26:25 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.Byju_Use_Case_Aj.Mobile}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.Custom activity.Mobile}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
