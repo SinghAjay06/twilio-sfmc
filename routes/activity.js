5dbeec786d39107dbdc7b6ab759af73dd1409baf
SinghAjay06 <92781365+SinghAjay06@users.noreply.github.com>
2022-02-23 14:32:44 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -157,9 +157,9 @@ exports.publish = function (req, res) {
     //console.log("Published: "+req.body.inArguments[0]);        
     
     // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-//     logData(req);
-//     res.send(200, 'Publish');
+    console.log( req.body );
+    logData(req);
+    res.send(200, 'Publish');
 };
 
 /*
